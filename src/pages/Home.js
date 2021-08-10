90f1b4ab47023446049b490f05afc4637c9799ff
zmeyer44 <meyerz@wustl.edu>
2021-08-10 08:41:19 -0400

    forgot to remove antd import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import "antd";
 import "./home.styles.css";
 import { images } from "../assets";
 
